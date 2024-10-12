4cc8e35c8b61f1c93725f7d50147f3217fdac64b
abhay.prajapati <abhay.prajapati@deskera.org>
2024-10-12 23:08:27 +0530

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -45,15 +45,6 @@ export function BlurImage() {
   return (
     <div className="flex justify-center items-center w-full">
       <div className="relative max-w-[min(100%,_1200px)]">
-        <img
-          alt="Majestic picture of a human using Cursor."
-          fetchPriority="high"
-          width="1800"
-          height="900"
-          decoding="async"
-          className="transition-opacity duration-500 ease-in-out opacity-100 max-w-[min(100%,_1200px)]"
-          src="https://anysphere.inc/_next/image?url=%2Fbg1.png&w=1920&q=75"
-        />
         <div className="absolute bottom-0 left-0 right-0 h-[10%] bg-gradient-to-t from-[hsl(222,21%,12%)] to-transparent z-10"></div>
       </div>
     </div>
