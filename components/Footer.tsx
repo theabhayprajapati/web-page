56c3328c9b0fc7dea02efe4472162184e03f3c41
theabhayprajapati <theabhayprajapati@gmail.com>
2024-12-05 09:16:27 +0530

    added resume

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <div className="absolute bottom-0 w-full h-[60px] flex justify-center items-center">
         <footer>
           <div className="grid place-content-center">
-            <div className="grid grid-cols-1 min-[500px]:grid-cols-3 items-center text-center text-xs text-[#DAD6C8] gap-12">
+            <div className="grid grid-cols-1 min-[500px]:grid-cols-4 items-center text-center text-xs text-[#DAD6C8] gap-12">
               <a className="relative w-[5.25rem]" href="https://github.com/theabhayprajapati">
               GitHub  
               </a>
@@ -21,6 +21,14 @@ export default function Footer() {
                 style={{ fontSize: "11px" }}
               >
                 @abhayprajapati_
+              </a>
+              {/* Resume */}
+              <a
+                href="/resume.pdf"
+                className="hover:text-newdimhover text-newdim hover:transition hover:duration-500 hidden min-[500px]:block"
+                style={{ fontSize: "11px" }}
+              >
+                #resume
               </a>
             </div>
           </div>
